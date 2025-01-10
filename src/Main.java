e7c371cbec5f2e9a996eb29459e5d7aba226bc23
joonseokchung <joonseok99@naver.com>
2025-01-10 18:38:34 +0900

    docs/update readme

--- src/Main.java
+++ src/Main.java
@@ -3,69 +3,73 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
+
 public class Main {
     public static void main(String[] args) throws IOException {
-        if(args.length < 2 || args.length > 5){
+        if (args.length < 2 || args.length > 5) {
             System.out.println("Usage: java Scheduling/Main <inputFileName> <schedulingAlgorithm>");
             System.out.println("available SchedulingAlgorithm : FCFS, SJF, RR with quantum 1/10/100");
-            return ;
+            return;
         }
 
-        //get infos from file
+        // get infos from file
         try {
             BufferedReader br = new BufferedReader(new FileReader(args[0]));
-            StringTokenizer st = new StringTokenizer(br.readLine(),"() ");
+            StringTokenizer st = new StringTokenizer(br.readLine(), "() ");
             br.close();
-            int processNum = Integer.parseInt(st.nextToken());  //number of process
-            //create process followed by info that got from file
+            int processNum = Integer.parseInt(st.nextToken()); // number of process
+            // create process followed by info that got from file
             pcb[] processes = new pcb[processNum];
-            for(int i = 0;i<processNum;i++){
-                processes[i] = new pcb(2023+i,Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
+            for (int i = 0; i < processNum; i++) {
+                processes[i] = new pcb(2023 + i, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
+                        Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
             }
-            String schedulingMethod = args[1];     //get scheduling method name
-            int quantum = 0;                       //using in RR
-            try{
-                if(args.length>3){
+            String schedulingMethod = args[1]; // get scheduling method name
+            int quantum = 0; // using in RR
+            try {
+                if (args.length > 3) {
                     quantum = Integer.parseInt(args[4]);
                 }
-            }
-            catch (NumberFormatException ex){
+            } catch (NumberFormatException ex) {
                 System.out.println("incorrect quantum");
                 return;
-            }
-            catch (ArrayIndexOutOfBoundsException ex){
+            } catch (ArrayIndexOutOfBoundsException ex) {
                 System.out.println("Usage: java Scheduling/Main <inputFileName> <schedulingAlgorithm>");
                 System.out.println("available SchedulingAlgorithm : FCFS, SJF, RR with quantum 1/10/100");
                 return;
             }
-            //variables that set strategy of Scheduler and array that will contain processes
+            // variables that set strategy of Scheduler and array that will contain
+            // processes
             scheduling s_method = null;
             queuing q_method = null;
-            //get strategy variables
-            switch(schedulingMethod){
-                case("FCFS"): {
+            // get strategy variables
+            switch (schedulingMethod) {
+                case ("FCFS"): {
                     s_method = new FCFSorSJF();
                     q_method = new q_sch();
-                    break;}
-                case("SJF"): {
+                    break;
+                }
+                case ("SJF"): {
                     s_method = new FCFSorSJF();
                     q_method = new pq_sch();
-                    break;}
-                case("RR"): {
+                    break;
+                }
+                case ("RR"): {
                     s_method = new RR();
                     q_method = new q_sch();
-                    break;}
+                    break;
+                }
                 default: {
                     System.out.println("Scheduling method name incorrect");
                     System.out.println("available SchedulingAlgorithm : FCFS, SJF, RR with quantum 1/10/100");
-                    return;}
+                    return;
+                }
             }
-            //execute scheduler
-            System.out.println("\nScheduling "+processNum+" processes by "+schedulingMethod);
+            // execute scheduler
+            System.out.println("\nScheduling " + processNum + " processes by " + schedulingMethod);
             schedule My_Scheduler = new schedule(processes);
-            My_Scheduler.exe(s_method,q_method,quantum);
-        }
-        catch(FileNotFoundException e) {
+            My_Scheduler.exe(s_method, q_method, quantum);
+        } catch (FileNotFoundException e) {
             System.out.println("File not Found");
             System.out.println("write correct file name");
         }
--- src/pcb.java
+++ src/pcb.java
@@ -3,15 +3,20 @@ import java.util.Random;
 
 public class pcb implements Comparable<pcb> {
     private int pid;
-    private int arrivalT, totalCPUT, CPUburst, IOburst; // arrival time, total CPU time, Maximum CPU burst time, Maximum IO burst time
+    private int arrivalT, totalCPUT, CPUburst, IOburst; // arrival time, total CPU time, Maximum CPU burst time, Maximum
+                                                        // IO burst time
     private int quantum, RemainingQuantum; // Using in round-robin
-    private int remainingCPUT, randomizedCPUburst, randomizedIOburst; //remaining cpu time, randomized cpu burst , randomized io burst
-    //Finishing time , Turnaround time, CPU time (time that remained on running state)
-    //IO time (time that remained on blocked state), Waiting time (time that remained on ready state)
+    private int remainingCPUT, randomizedCPUburst, randomizedIOburst; // remaining cpu time, randomized cpu burst ,
+                                                                      // randomized io burst
+    // Finishing time , Turnaround time, CPU time (time that remained on running
+    // state)
+    // IO time (time that remained on blocked state), Waiting time (time that
+    // remained on ready state)
     private int finishingT, turnaroundT, CPUT, IOT, waitingT = 0;
     private state processState; // process state
     private Random random = new Random();
-    public pcb(int id,int A,int C, int B, int IO){
+
+    public pcb(int id, int A, int C, int B, int IO) {
         this.pid = id;
         this.arrivalT = A;
         this.totalCPUT = C;
@@ -20,179 +25,234 @@ public class pcb implements Comparable<pcb> {
         this.IOburst = IO;
         this.processState = New.getInstance();
     }
-    public void setProcessState(state s){
+
+    public void setProcessState(state s) {
         this.processState = s;
     }
-    public void addTime(){
+
+    public void addTime() {
         processState.addTime(this);
     }
-    //put random number up to given Maximum CPU/IO time
-    public void randomBurst(){
-        if(this.CPUburst >0) {
+
+    // put random number up to given Maximum CPU/IO time
+    public void randomBurst() {
+        if (this.CPUburst > 0) {
             this.randomizedCPUburst = random.nextInt(this.CPUburst) + 1;
-        }
-        else{
+        } else {
             this.randomizedCPUburst = this.CPUburst;
         }
-        if(this.IOburst >0){
-            this.randomizedIOburst = random.nextInt(this.IOburst)+1;
+        if (this.IOburst > 0) {
+            this.randomizedIOburst = random.nextInt(this.IOburst) + 1;
 
-        }
-        else{
+        } else {
             this.randomizedIOburst = this.IOburst;
         }
     }
-    //print process summary
-    public void printInfo(){
-        System.out.println("---\t-- pid = "+this.pid+" --\t---");
-        System.out.println("Arrival time : "+this.arrivalT +", Total CPU time : "+this.totalCPUT +", CPU burst bound : "+this.CPUburst +", IO burst bound : "+this.IOburst);
-        System.out.println("Finishing time : "+this.finishingT +", Turnaround time : "+this.turnaroundT);
-        System.out.println("CPU time : "+this.CPUT +", IO time : "+this.IOT +", Waiting time : "+this.waitingT);
+
+    // print process summary
+    public void printInfo() {
+        System.out.println("---\t-- pid = " + this.pid + " --\t---");
+        System.out.println("Arrival time : " + this.arrivalT + ", Total CPU time : " + this.totalCPUT
+                + ", CPU burst bound : " + this.CPUburst + ", IO burst bound : " + this.IOburst);
+        System.out.println("Finishing time : " + this.finishingT + ", Turnaround time : " + this.turnaroundT);
+        System.out.println("CPU time : " + this.CPUT + ", IO time : " + this.IOT + ", Waiting time : " + this.waitingT);
         System.out.println("---\t---\t---\t---\t---\t---");
     }
-    public boolean isRunning(){
-        if(Objects.equals(this.processState, running.getInstance())){
+
+    public boolean isRunning() {
+        if (Objects.equals(this.processState, running.getInstance())) {
             return true;
         }
         return false;
     }
-    public boolean isWaiting(){
-        if(Objects.equals(this.processState, waiting.getInstance())){
+
+    public boolean isWaiting() {
+        if (Objects.equals(this.processState, waiting.getInstance())) {
             return true;
         }
         return false;
     }
-    //need to compare processes when adding process to priority queue
-    public int getPid(){
+
+    // need to compare processes when adding process to priority queue
+    public int getPid() {
         return this.pid;
     }
-    public int getArrivalT(){
+
+    public int getArrivalT() {
         return this.arrivalT;
     }
-    public void setQuantum(int q){
+
+    public void setQuantum(int q) {
         this.quantum = q;
     }
-    public void initRemainingQuantum(){
+
+    public void initRemainingQuantum() {
         this.RemainingQuantum = this.quantum;
     }
-    public int getRemainingQuantum(){
+
+    public int getRemainingQuantum() {
         return this.RemainingQuantum;
     }
-    public void setRemainingQuantum(int q){
+
+    public void setRemainingQuantum(int q) {
         this.RemainingQuantum = q;
     }
-    public int getRemainingCPUT(){
+
+    public int getRemainingCPUT() {
         return this.remainingCPUT;
     }
-    public void setRemainingCPUT(int t){
+
+    public void setRemainingCPUT(int t) {
         this.remainingCPUT = t;
     }
-    public int getRandomizedCPUburst(){
+
+    public int getRandomizedCPUburst() {
         return this.randomizedCPUburst;
     }
-    public void setRandomizedCPUburst(int t){
+
+    public void setRandomizedCPUburst(int t) {
         this.randomizedCPUburst = t;
     }
-    public int getRandomizedIOburst(){
+
+    public int getRandomizedIOburst() {
         return this.randomizedIOburst;
     }
-    public void setRandomizedIOburst(int t){
+
+    public void setRandomizedIOburst(int t) {
         this.randomizedIOburst = t;
     }
-    public int getFinishingT(){
+
+    public int getFinishingT() {
         return this.finishingT;
     }
-    public void setFinishingT(int t){
+
+    public void setFinishingT(int t) {
         this.finishingT = t;
     }
-    public int getTurnaroundT(){
-        this.turnaroundT = this.finishingT -this.arrivalT;
+
+    public int getTurnaroundT() {
+        this.turnaroundT = this.finishingT - this.arrivalT;
         return this.turnaroundT;
     }
-    public int getCPUT(){
+
+    public int getCPUT() {
         return this.CPUT;
     }
-    public void setCPUT(int t){
+
+    public void setCPUT(int t) {
         this.CPUT = t;
     }
-    public int getIOT(){
+
+    public int getIOT() {
         return this.IOT;
     }
-    public void setIOT(int t){
+
+    public void setIOT(int t) {
         this.IOT = t;
     }
-    public int getWaitingT(){
+
+    public int getWaitingT() {
         return this.waitingT;
     }
-    public void setWaitingT(int t){
+
+    public void setWaitingT(int t) {
         this.waitingT = t;
     }
 
     @Override
-    public int compareTo(pcb p) {       //using when enqueuing to priority queue
-        if(this.randomizedCPUburst < p.getRandomizedCPUburst()) return -1;
-        else if(this.randomizedCPUburst >p.getRandomizedCPUburst()) return 1;
+    public int compareTo(pcb p) { // using when enqueuing to priority queue
+        if (this.randomizedCPUburst < p.getRandomizedCPUburst())
+            return -1;
+        else if (this.randomizedCPUburst > p.getRandomizedCPUburst())
+            return 1;
         return 0;
     }
 }
+
 interface state {
-    //adjust time to each variable according to its state
+    // adjust time to each variable according to its state
     public void addTime(pcb p);
 }
+
 class New implements state {
     private static New New = new New();
-    private New(){}
-    public static New getInstance(){
+
+    private New() {
+    }
+
+    public static New getInstance() {
         return New;
     }
+
     public void addTime(pcb p) {
-        p.setFinishingT(p.getFinishingT()+1);
+        p.setFinishingT(p.getFinishingT() + 1);
     }
 }
+
 class ready implements state {
     private static ready ready = new ready();
-    private ready(){}
-    public static ready getInstance(){
+
+    private ready() {
+    }
+
+    public static ready getInstance() {
         return ready;
     }
+
     public void addTime(pcb p) {
-        p.setFinishingT(p.getFinishingT()+1);
-        p.setWaitingT(p.getWaitingT()+1);
+        p.setFinishingT(p.getFinishingT() + 1);
+        p.setWaitingT(p.getWaitingT() + 1);
         p.initRemainingQuantum();
     }
 }
+
 class running implements state {
     private static running running = new running();
-    private running(){}
-    public static running getInstance(){
+
+    private running() {
+    }
+
+    public static running getInstance() {
         return running;
     }
+
     public void addTime(pcb p) {
-        p.setFinishingT(p.getFinishingT()+1);
-        p.setCPUT(p.getCPUT()+1);
-        p.setRemainingCPUT(p.getRemainingCPUT()-1);
-        p.setRandomizedCPUburst(p.getRandomizedCPUburst()-1);
-        p.setRemainingQuantum(p.getRemainingQuantum()-1);
+        p.setFinishingT(p.getFinishingT() + 1);
+        p.setCPUT(p.getCPUT() + 1);
+        p.setRemainingCPUT(p.getRemainingCPUT() - 1);
+        p.setRandomizedCPUburst(p.getRandomizedCPUburst() - 1);
+        p.setRemainingQuantum(p.getRemainingQuantum() - 1);
     }
 }
+
 class waiting implements state {
-    private static waiting waiting= new waiting();
-    private waiting(){}
-    public static waiting getInstance(){
+    private static waiting waiting = new waiting();
+
+    private waiting() {
+    }
+
+    public static waiting getInstance() {
         return waiting;
     }
+
     public void addTime(pcb p) {
-        p.setFinishingT(p.getFinishingT()+1);
-        p.setIOT(p.getIOT()+1);
-        p.setRandomizedIOburst(p.getRandomizedIOburst()-1);
+        p.setFinishingT(p.getFinishingT() + 1);
+        p.setIOT(p.getIOT() + 1);
+        p.setRandomizedIOburst(p.getRandomizedIOburst() - 1);
         p.initRemainingQuantum();
     }
 }
+
 class terminated implements state {
     private static terminated terminated = new terminated();
-    private terminated(){}
-    public static terminated getInstance(){
+
+    private terminated() {
+    }
+
+    public static terminated getInstance() {
         return terminated;
     }
-    public void addTime(pcb p) {}
+
+    public void addTime(pcb p) {
+    }
 }
